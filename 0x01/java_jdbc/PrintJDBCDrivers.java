9a0dacb7ef0d417430730066fadda66b2324d621
aleferamos <alefepdias@gmail.com>
2022-06-08 14:39:25 -0400

    t

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -4,8 +4,7 @@ import java.util.stream.Collectors;
 public class PrintJDBCDrivers {
     public static void main(String[] args) {
 
-        var driver = DriverManager.getDrivers().nextElement();
-        System.out.println(driver.getClass().getName() + " " + driver.getMajorVersion() + "." + driver.getMinorVersion());
-        
+        var driver = DriverManager.getDrivers();
+
     }
 }
